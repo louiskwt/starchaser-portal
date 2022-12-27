a9d5e8590be971855139c5adade3e43b900a1669
Louis Tsang <kawingt2@gmail.com>
2022-12-27 23:15:51 +0800

    built login form

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,72 @@
 const LoginPage = () => {
   return (
-    <div className="container w-100 mx-auto text-center p-8">
-      <h1 className="text-3xl font-bold ">Log in!</h1>
-    </div>
+    <section className="h-screen">
+      <div className="container px-6 py-12 h-full">
+        <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+          <div className="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+              alt="login-image"
+              className="w-full"
+            />
+          </div>
+          <div className="md:w-8/12 lg:w-5/12 lg:ml-20">
+            <h1 className="text-3xl text-gray-800 text-center font-semibold mb-8">
+              Login
+            </h1>
+            <form>
+              <div className="mb-6">
+                <input
+                  type="email"
+                  name="email"
+                  className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  placeholder="Email Address"
+                />
+              </div>
+              <div className="mb-6">
+                <input
+                  type="password"
+                  className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  placeholder="Password"
+                />
+              </div>
+              <div className="flex justify-between items-center mb-6">
+                <div className="form-group form-check mr-4">
+                  <input
+                    id="rememberMe"
+                    type="checkbox"
+                    value=""
+                    className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                  ></input>
+
+                  <label
+                    className="form-check-label inline-block text-gray-800"
+                    htmlFor="rememberMe"
+                  >
+                    Remember me
+                  </label>
+                </div>
+                <a
+                  href="#!"
+                  className="text-blue-600 hover:text-blue-700 focus:text-blue-700 active:text-blue-800 duration-200 transition ease-in-out"
+                >
+                  Forgot password?
+                </a>
+              </div>
+
+              <button
+                type="submit"
+                className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
+                data-mdb-ripple="true"
+                data-mdb-ripple-color="light"
+              >
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
