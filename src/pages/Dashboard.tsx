16de50b185824f6e17da320a7a3dd89cd959a5b7
Louis Tsang <kawingt2@gmail.com>
2022-12-28 23:00:10 +0800

    implemented protected route

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,16 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 const Dashboard = () => {
   const { user } = useAuth();
   const navigate = useNavigate();
-  if (!user) {
-    navigate("/login");
-  }
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, []);
+
   return (
     <div className="container w-100 mx-auto text-center p-8">
       <h1 className="text-3xl font-bold ">Hello world!</h1>
