a3c8484a9629b6e07ad2111c08351c0bff1c040d
Louis Tsang <kawingt2@gmail.com>
2022-12-28 22:57:58 +0800

    fixed env variables

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 export const config = {
   // firebase config
   firebaseConfig: {
-    apiKey: import.meta.env.FIREBASE_API_KEY,
-    authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.FIREBASE_BUCKET,
-    messagingSenderId: import.meta.env.FIREBASE_SENDER_ID,
-    appId: import.meta.env.FIREBASE_APP_ID,
-    measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID,
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
   },
 };
 
