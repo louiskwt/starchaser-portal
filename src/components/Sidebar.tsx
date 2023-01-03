d9b9d41c1af7f13012f393aea224a5f1172b509f
Louis Tsang <kawingt2@gmail.com>
2023-01-03 23:02:51 +0800

    cleaned up add fonticon and set up sidebar links

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -23,6 +23,28 @@ const Sidebar = () => {
             <li className="mr-3 flex-1">
               <a
                 href="#"
+                className="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-green-500"
+              >
+                <i className="fas fa-book-open pr-0 md:pr-3"></i>
+                <span className="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">
+                  Examplars
+                </span>
+              </a>
+            </li>
+            <li className="mr-3 flex-1">
+              <a
+                href="#"
+                className="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-yellow-500"
+              >
+                <i className="fas fa-book pr-0 md:pr-3"></i>
+                <span className="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">
+                  Vocab Banks
+                </span>
+              </a>
+            </li>
+            <li className="mr-3 flex-1">
+              <a
+                href="#"
                 className="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-purple-500"
               >
                 <i className="fa fa-envelope pr-0 md:pr-3"></i>
