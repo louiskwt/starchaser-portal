e07dbeee507b5c567b2e1c7b4e7a0631124c2a70
Louis Tsang <kawingt2@gmail.com>
2023-01-07 23:05:03 +0800

    fixed loader

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -10,55 +10,58 @@ const Dashboard = () => {
   return (
     <>
       <Navbar />
-
-      {!user ? (
-        <Loader />
-      ) : (
-        <main>
-          <div className="flex flex-col md:flex-row">
-            <Sidebar />
-            <section className="w-full">
-              <div
-                id="main"
-                className="main-content w-full h-screen flex-1 bg-gray-100 mt-12 md:mt-2 pb-24 md:pb-5"
-              >
-                <div className="bg-gray-800 pt-3">
-                  <div className="rounded-tl-3xl bg-gradient-to-r from-blue-900 to-gray-800 p-4 shadow text-2xl text-white">
-                    <h1 className="font-bold pl-2">Analytics</h1>
-                  </div>
-                </div>
-                <div className="flex flex-wrap">
-                  <MetricCard
-                    title="Weekly Target"
-                    data="1 / 2 Completed"
-                    cardStyle={cardStyles.green}
-                    textStyle={textStyles.dark}
-                    iconStyle={iconStyles.green}
-                    icon="calendar"
-                  />
-                  <MetricCard
-                    title="Days to DSE"
-                    data="106 days"
-                    cardStyle={cardStyles.blue}
-                    textStyle={textStyles.dark}
-                    iconStyle={iconStyles.blue}
-                    icon="calendar"
-                  />
-                </div>
-                <button
-                  type="button"
-                  className="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  onClick={() => {
-                    logOut();
-                  }}
+      <main>
+        <div className="flex flex-col md:flex-row">
+          {!user ? (
+            <div className="flex flex-col mt-80 mx-auto">
+              <Loader />
+            </div>
+          ) : (
+            <>
+              <Sidebar />
+              <section className="w-full">
+                <div
+                  id="main"
+                  className="main-content w-full h-screen flex-1 bg-gray-100 mt-12 md:mt-2 pb-24 md:pb-5"
                 >
-                  Logout
-                </button>
-              </div>
-            </section>
-          </div>
-        </main>
-      )}
+                  <div className="bg-gray-800 pt-3">
+                    <div className="rounded-tl-3xl bg-gradient-to-r from-blue-900 to-gray-800 p-4 shadow text-2xl text-white">
+                      <h1 className="font-bold pl-2">Analytics</h1>
+                    </div>
+                  </div>
+                  <div className="flex flex-wrap">
+                    <MetricCard
+                      title="Weekly Target"
+                      data="1 / 2 Completed"
+                      cardStyle={cardStyles.green}
+                      textStyle={textStyles.dark}
+                      iconStyle={iconStyles.green}
+                      icon="calendar"
+                    />
+                    <MetricCard
+                      title="Days to DSE"
+                      data="106 days"
+                      cardStyle={cardStyles.blue}
+                      textStyle={textStyles.dark}
+                      iconStyle={iconStyles.blue}
+                      icon="calendar"
+                    />
+                  </div>
+                  <button
+                    type="button"
+                    className="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    onClick={() => {
+                      logOut();
+                    }}
+                  >
+                    Logout
+                  </button>
+                </div>
+              </section>
+            </>
+          )}
+        </div>
+      </main>
     </>
   );
 };
