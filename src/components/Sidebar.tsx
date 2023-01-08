c35a94c56a53dca1ff5d62e3ad9a0dedeff0e78c
Louis Tsang <kawingt2@gmail.com>
2023-01-08 13:15:11 +0800

    optimised links

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,75 +1,72 @@
+import { useLocation } from "react-router-dom";
+
+const sideBarLinkData = [
+  {
+    name: "Home",
+    icon: "fas fa-home",
+    path: "/",
+    color: "green",
+  },
+  {
+    name: "Task",
+    icon: "fas fa-tasks",
+    path: "/task",
+    color: "pink",
+  },
+  {
+    name: "Vocab",
+    icon: "fas fa-book",
+    path: "/vocab",
+    color: "yellow",
+  },
+  {
+    name: "Payment",
+    icon: "fas fa-wallet",
+    path: "/payment",
+    color: "purple",
+  },
+];
+
+const generateSidebarButton = (
+  name: string,
+  icon: string,
+  path: string,
+  color: string,
+  currentPath: string
+) => {
+  return (
+    <li className="mr-3 flex-1">
+      <a
+        href={path}
+        className={
+          currentPath === path
+            ? `sidebarLink sidebar-${color} sidebarActive-${color}`
+            : `sidebarLink sidebar-${color}`
+        }
+      >
+        <i className={`${icon} sidebarIcon`}></i>
+        <span className="sidebarText">{name}</span>
+      </a>
+    </li>
+  );
+};
+
 const Sidebar = () => {
+  const location = useLocation();
   return (
     <nav aria-label="alternative nav">
       <div className="bg-gray-800 shadow-xl h-20 fixed bottom-0 mt-12 md:relative md:h-screen z-10 w-full md:w-48 content-center">
         <div className="md:mt-12 md:w-48 md:fixed md:left-0 md:top-0 content-center md:content-start text-left justify-between">
           <ul className="list-reset flex flex-row md:flex-col pt-3 md:py-3 px-1 md:px-2 text-center md:text-left">
-            <li className="mr-3 flex-1">
-              <a
-                href="#"
-                className="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-pink-500"
-              >
-                <i className="fas fa-tasks pr-0 md:pr-3"></i>
-                <span className="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">
-                  Tasks
-                </span>
-              </a>
-            </li>
-            <li className="mr-3 flex-1">
-              <a
-                href="#"
-                className="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-green-500"
-              >
-                <i className="fas fa-book-open pr-0 md:pr-3"></i>
-                <span className="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">
-                  Examplars
-                </span>
-              </a>
-            </li>
-            <li className="mr-3 flex-1">
-              <a
-                href="#"
-                className="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-yellow-500"
-              >
-                <i className="fas fa-book pr-0 md:pr-3"></i>
-                <span className="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">
-                  Vocab
-                </span>
-              </a>
-            </li>
-            <li className="mr-3 flex-1">
-              <a
-                href="#"
-                className="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-purple-500"
-              >
-                <i className="fa fa-envelope pr-0 md:pr-3"></i>
-                <span className="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">
-                  Messages
-                </span>
-              </a>
-            </li>
-            <li className="mr-3 flex-1">
-              <a
-                href="#"
-                className="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-blue-600"
-              >
-                <i className="fas fa-chart-area pr-0 md:pr-3"></i>
-                <span className="pb-1 md:pb-0 text-xs md:text-base text-white md:text-white block md:inline-block">
-                  Analytics
-                </span>
-              </a>
-            </li>
-            <li className="mr-3 flex-1">
-              <a
-                href="#"
-                className="block py-1 md:py-3 pl-0 md:pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-red-500"
-              >
-                <i className="fa fa-wallet pr-0 md:pr-3"></i>
-                <span className="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">
-                  Payments
-                </span>
-              </a>
-            </li>
+            {sideBarLinkData.map((link) => {
+              return generateSidebarButton(
+                link.name,
+                link.icon,
+                link.path,
+                link.color,
+                location.pathname
+              );
+            })}
           </ul>
         </div>
       </div>
