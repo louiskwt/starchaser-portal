aea329ee1584debedf4d3d0d7a0333bff10d3e0c
Louis Tsang <kawingt2@gmail.com>
2023-01-12 23:12:02 +0800

    fixed table style and mock table

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -16,12 +16,14 @@ const Task = () => {
             <div className="rounded-t mb-0 px-4 py-3 border-0">
               <div className="flex flex-wrap items-center">
                 <div className="relative w-full px-4 max-w-full flex-grow flex-1">
-                  <h3 className="font-semibold text-base text-blueGray-700">
+                  <h3 className="font-semibold text-base text-gray-700">
                     Tasks
                   </h3>
                 </div>
                 <div className="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
-                  <h5>1/8 - 1/15</h5>
+                  <h5 className="font-semibold text-base text-gray-700">
+                    Date: 1/8 - 1/15
+                  </h5>
                 </div>
               </div>
             </div>
@@ -29,39 +31,65 @@ const Task = () => {
               <table className="items-center w-full border-collapse text-blueGray-700 ">
                 <thead className="thead-light ">
                   <tr>
-                    <th className="px-6 w-2 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    <th className="px-6 w-4 bg-gray-50 text-gray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                       #
                     </th>
-                    <th className="px-6 w-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    <th className="px-6 w-4 bg-gray-50 text-gray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                       Task
                     </th>
-                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    <th className="px-6 w-6 bg-gray-50 text-gray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                       Due Date
                     </th>
-                    <th className="px-6 bg-blueGray-50 text-blueGray-700 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left min-w-140-px"></th>
+                    <th className="px-6 w-6 bg-gray-50 text-gray-700 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left min-w-140-px">
+                      Status
+                    </th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr>
                     <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-                      Facebook
+                      1
                     </th>
                     <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-                      Facebook
+                      Paper 2
                     </th>
-                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
-                      1,480
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      1 / 12 / 2023
                     </td>
                     <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
                       <div className="flex items-center">
-                        <span className="mr-2">60%</span>
-                        <div className="relative w-full">
-                          <div className="overflow-hidden h-2 text-xs flex rounded bg-red-200">
-                            <div
-                              style={{ width: "60%" }}
-                              className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-red-500"
-                            ></div>
-                          </div>
+                        <div
+                          style={{
+                            paddingTop: "0.1em",
+                            paddingBottom: "0.1rem",
+                          }}
+                          className="text-sm px-3 bg-red-200 text-red-800 rounded-full"
+                        >
+                          Not Yet Submitted
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                      2
+                    </th>
+                    <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                      Paper 2
+                    </th>
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      1 / 12 / 2023
+                    </td>
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      <div className="flex items-center">
+                        <div
+                          style={{
+                            paddingTop: "0.1em",
+                            paddingBottom: "0.1rem",
+                          }}
+                          className="text-sm px-3 bg-red-200 text-red-800 rounded-full"
+                        >
+                          Not Yet Submitted
                         </div>
                       </div>
                     </td>
