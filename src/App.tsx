30108232b74043d7c9ba228f51c97535236378f1
Louis Tsang <kawingt2@gmail.com>
2023-01-12 23:30:58 +0800

    set up payment page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { AuthProvider } from "./context/AuthContext";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import PaymentPage from "./pages/PaymentPage";
 import ProfileSetUpPage from "./pages/ProfileSetUpPage";
 import SignUpPage from "./pages/SignUpPage";
 import TaskPage from "./pages/TaskPage";
@@ -12,10 +13,11 @@ function App(): JSX.Element {
   return (
     <AuthProvider>
       <Routes>
-        <Route path="/task" element={<TaskPage />} />
         <Route path="/" element={<HomePage />} />
+        <Route path="task" element={<TaskPage />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="signup" element={<SignUpPage />} />
+        <Route path="payment" element={<PaymentPage />} />
         <Route path="set-profile" element={<ProfileSetUpPage />} />
       </Routes>
       <ToastContainer />
---
+++ src/pages/PaymentPage.tsx
@@ -0,0 +1,8 @@
+import DashboardLayout from "../components/DashboardLayout";
+import Payment from "../components/Payment";
+
+const PaymentPage = () => {
+  return <DashboardLayout children={<Payment />} />;
+};
+
+export default PaymentPage;
