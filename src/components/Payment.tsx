9f8ff4b1b4d90623ee789eba311240b279dd0d5b
Louis Tsang <kawingt2@gmail.com>
2023-01-14 21:23:12 +0800

    fixed payment page style

--- src/components/Payment.tsx
+++ src/components/Payment.tsx
@@ -15,26 +15,26 @@ const Payment = () => {
         </div>
         <div></div>
         <div className="w-full px-16 mx-auto mt-12">
-          <h1 className="mb-2 text-xl font-semibold text-gray-900 ">
+          <h1 className="mb-2 text-xl font-bold text-gray-900 ">
             學費繳付方式:
           </h1>
           <ol className="max-w-md space-y-1 mb-6 text-gray-800 list-decimal list-inside">
-            <li className="mt-12 w-100 mb-12">
+            <li className="mt-12 w-100 mb-12 font-semibold">
               Pay Me
-              <div className="flex justify-center align-middle mt-4 ml-32 rounded border border-red-300">
+              <div className="flex justify-center align-middle mt-4  md:ml-32 rounded border border-red-300">
                 <img src={payme} alt="payMe QR code" />
               </div>
             </li>
-            <li className="mt-12 w-100 mb-12">
+            <li className="mt-12 w-100 mb-12 font-semibold">
               Alipay
-              <div className="flex justify-center align-middle mt-4 ml-32 rounded border border-blue-300">
+              <div className="flex justify-center align-middle mt-4  md:ml-32 rounded border border-blue-300">
                 <img src={alipay} alt="payMe QR code" />
               </div>
             </li>
             <br />
-            <li className="mt-24 w-100 mb-4">
+            <li className="mt-24 w-100 mb-4 font-semibold">
               FPS / Bank Transfer
-              <div className="flex flex-col mt-4 ml-32 rounded border bg-gray-100 border-green-300 p-3">
+              <div className="flex flex-col mt-4  md:ml-32 rounded border bg-gray-50 border-green-300 p-3">
                 <p className="font-bold w-100">FPS: +852 6352 0220</p>
                 <p className="font-bold w-100 mt-2">
                   Bank Transfer: 394-470108-882
