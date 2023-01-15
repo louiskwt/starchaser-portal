2e0a16ff3fa397cf0e1d5bf58910b90285a088d1
Louis Tsang <kawingt2@gmail.com>
2023-01-15 22:50:15 +0800

    cleaned up Home Page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,4 @@
 import { cardStyles, iconStyles, textStyles } from "../styles/tailwindClasses";
-import InfoCard from "./InfoCard";
 import MetricCard from "./MetricCard";
 
 const Home = () => {
@@ -31,14 +30,7 @@ const Home = () => {
             iconStyle={iconStyles.blue}
             icon="clock"
           />
-          <MetricCard
-            title="Weekly Progress"
-            data="30%"
-            cardStyle={cardStyles.red}
-            textStyle={textStyles.dark}
-            iconStyle={iconStyles.red}
-            icon="tasks"
-          />
+
           <MetricCard
             title="Points"
             data="100"
@@ -48,8 +40,93 @@ const Home = () => {
             icon="star"
           />
         </div>
-        <div className="flex flex-row flex-wrap flex-grow mt-2">
-          <InfoCard title={"Quote"} data={"To dare is to dream"} />
+        <div className="w-full px-4 mx-auto mt-12">
+          <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-white ">
+            <div className="rounded-t mb-0 px-4 py-3 border-0">
+              <div className="flex flex-wrap items-center">
+                <div className="relative w-full px-4 max-w-full flex-grow flex-1">
+                  <h3 className="font-semibold text-base text-gray-700">
+                    Tasks
+                  </h3>
+                </div>
+                <div className="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
+                  <h5 className="font-semibold text-base text-gray-700">
+                    Date: 1/8 - 1/15
+                  </h5>
+                </div>
+              </div>
+            </div>
+            <div className="w-full overflow-x-auto">
+              <table className="items-center w-full border-collapse text-blueGray-700 ">
+                <thead className="thead-light ">
+                  <tr>
+                    <th className="px-6 w-4 bg-gray-50 text-gray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                      #
+                    </th>
+                    <th className="px-6 w-4 bg-gray-50 text-gray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                      Task
+                    </th>
+                    <th className="px-6 w-6 bg-gray-50 text-gray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                      Due Date
+                    </th>
+                    <th className="px-6 w-6 bg-gray-50 text-gray-700 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left min-w-140-px">
+                      Status
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                      1
+                    </th>
+                    <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                      Paper 2
+                    </th>
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      1 / 12 / 2023
+                    </td>
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      <div className="flex items-center">
+                        <div
+                          style={{
+                            paddingTop: "0.1em",
+                            paddingBottom: "0.1rem",
+                          }}
+                          className="text-sm px-3 bg-red-200 text-red-800 rounded-full"
+                        >
+                          Not Yet Submitted
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                      2
+                    </th>
+                    <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                      Paper 2
+                    </th>
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      1 / 12 / 2023
+                    </td>
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      <div className="flex items-center">
+                        <div
+                          style={{
+                            paddingTop: "0.1em",
+                            paddingBottom: "0.1rem",
+                          }}
+                          className="text-sm px-3 bg-red-200 text-red-800 rounded-full"
+                        >
+                          Not Yet Submitted
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     </section>
