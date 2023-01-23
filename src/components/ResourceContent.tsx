683e5cf6b880e8c6c0f1f07e34fc4566263eb6d2
Louis Tsang <kawingt2@gmail.com>
2023-01-23 23:12:55 +0800

    set up Resource Content Page

---
+++ src/components/ResourceContent.tsx
@@ -0,0 +1,24 @@
+const ResourceContent = () => {
+  return (
+    <section className="w-full">
+      <div
+        id="main"
+        className="main-content w-full h-full flex-1 bg-gray-100 mt-12 md:mt-2 pb-24 md:pb-5"
+      >
+        <div className="bg-gray-800 pt-3">
+          <div className="rounded-tl-3xl bg-gradient-to-r from-blue-900 to-gray-800 p-4 shadow text-2xl text-white">
+            <h1 className="font-bold pl-2">Title</h1>
+          </div>
+        </div>
+        <div></div>
+        <div className="w-full px-16 mx-auto mt-12">
+          <h1 className="mb-4 text-xl font-semibold text-gray-900 ">
+            精讀學習資源:
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ResourceContent;
---
+++ src/pages/ResourceContentPage.tsx
@@ -0,0 +1,8 @@
+import DashboardLayout from "../components/DashboardLayout";
+import ResourceContent from "../components/ResourceContent";
+
+const ResouceContentPage = () => {
+  return <DashboardLayout children={<ResourceContent />} />;
+};
+
+export default ResouceContentPage;
