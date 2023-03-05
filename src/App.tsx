98183ef95df9e03ae3b0ff8c30650ddf8826f246
Louis Tsang <kawingt2@gmail.com>
2023-03-05 23:03:52 +0800

    set up for Chinese UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import "react-toastify/dist/ReactToastify.css";
 import ErrorBoundry from "./components/ErrorBoundry";
 import { AuthProvider } from "./context/AuthContext";
 import { FirestoreProvider } from "./context/FirestoreContext";
+import enUS from "./locales/en-US.json";
+import zhTW from "./locales/zh-TW.json";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import PaymentPage from "./pages/PaymentPage";
@@ -12,6 +14,22 @@ import ResoucePage from "./pages/ResoucePage";
 import ResouceContentPage from "./pages/ResourceContentPage";
 import SignUpPage from "./pages/SignUpPage";
 import TaskPage from "./pages/TaskPage";
+
+const LOCALES_LIST = [
+  {
+    label: "English",
+    value: "en-US",
+  },
+  {
+    label: "繁體中文",
+    value: "zh-TW",
+  },
+];
+
+const LOCALE_DATA = {
+  "en-US": enUS,
+  "zh-TW": zhTW,
+};
 
 function App(): JSX.Element {
   return (
