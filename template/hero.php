45cb83b6879493645fcc79f05628a3886c5a3901
Louis Tsang <kawingt2@gmail.com>
2023-07-23 18:46:13 +0800

    back to php and build a simple site

---
+++ template/hero.php
@@ -0,0 +1,19 @@
+<?php
+  $v_link = "#";
+  $r_link = "#";
+?>
+
+<div class="bg-dark text-secondary px-4 py-5 text-center">
+    <div class="py-5">
+      <h1 class="display-5 fw-bold text-white">IELTS Open Course</h1>
+      <div class="col-lg-6 mx-auto">
+        <p class="fs-5 mb-4"> 只要你有心，一切都有可能！ </p>
+        <div class="d-grid gap-2 row-gap-4 d-sm-flex justify-content-sm-center ">
+        <a href=<?= $r_link ?> class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">
+            Notes and Slides
+        </a>
+        <a href=<?= $v_link ?>  class="btn btn-outline-light btn-lg px-4">Videos</a>
+        </div>
+      </div>
+    </div>
+</div>
