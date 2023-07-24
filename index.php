19a8cf59fa666567c276277b6c4582fcef87fe55
Louis Tsang <kawingt2@gmail.com>
2023-07-24 23:28:37 +0800

    added meta tags

--- index.php
+++ index.php
@@ -8,7 +8,10 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>StarChaser</title>
+    <title>StarChaser | 線上英文學習平台</title>
+    <meta content="線上 DSE 英文 及 IELTS 考試學習平台，提供練習及公開課程去幫你備考" name="description">
+    <meta content="線上 DSE 英文 及 IELTS 考試學習平台，提供練習及公開課程去幫你備考" property="og:description">
+    <meta content="StarChaser | 線上英文學習平台" property="og:title">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
   </head>
 
