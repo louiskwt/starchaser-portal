346b86350e8d64a228179af00db9151be94b4bdd
Louis Tsang <kawingt2@gmail.com>
2023-07-30 17:18:22 +0800

    simplified app

--- app/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-starchaser_app = Flask(__name__)
-
-
-from app import routes
--- app/routes.py
+++
@@ -1,7 +0,0 @@
-from app import starchaser_app 
-
-@starchaser_app.route('/')
-@starchaser_app.route('/index')
-def index():
-    return "Hello Starchaser"
-
