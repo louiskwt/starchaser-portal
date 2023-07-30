9f2e6bd5efdce8a483260a53c64ea7eab896edb8
Louis Tsang <kawingt2@gmail.com>
2023-07-30 17:55:33 +0800

    rendered html

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return '<h1>Hello Starchaser</h1>'
+    resource = 'https://drive.google.com/drive/folders/19t63LXgL-I1-SOj1aDvabBqUqv0E7Ng1?usp=sharing'
+    video = 'https://www.youtube.com/watch?v=FrQK56ojb6c&list=PLms_Uy6lFiB3y-oxHKe2zoUTUw3jfaBZ1'
+    return render_template('index.html', r_link=resource, v_link=video) 
