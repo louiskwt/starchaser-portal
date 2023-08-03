f91a818c7d9ed67da5be4953abb9d0d4fd8acfa6
Louis Tsang <kawingt2@gmail.com>
2023-08-03 23:27:35 +0800

    configured dse models

--- dse/models.py
+++ dse/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=256)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("notes_detail", kwargs={"pk": self.pjk})
