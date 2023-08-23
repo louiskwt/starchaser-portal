dd8aa7cf757427358dab10a5248b6849d05d0463
Louis Tsang <kawingt2@gmail.com>
2023-08-23 23:07:13 +0800

    updated config

--- starchaser_project/settings.py
+++ starchaser_project/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.getenv('IS_DEV')) == 1
 
-ALLOWED_HOSTS = ['www.dsestarchaser.me']
+ALLOWED_HOSTS = ['www.dsestarchaser.me', '127.0.0.1']
 
 
 # Application definition
