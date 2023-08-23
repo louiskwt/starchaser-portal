5ac2a965df013666000b424d18adb9819e63b170
Louis Tsang <kawingt2@gmail.com>
2023-08-23 23:20:08 +0800

    added and connected to db for starchaser

--- starchaser_project/settings.py
+++ starchaser_project/settings.py
@@ -80,8 +80,14 @@ WSGI_APPLICATION = 'starchaser_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': str(os.getenv('PG_NAME')),
+        'USER': str(os.getenv('PG_USER')),
+        'PASSWORD': str(os.getenv('PG_PW')),
+        'HOST': str(os.getenv('PG_HOST')),
+        'PORT': str(os.getenv('PG_PORT')),
     }
 }
 
