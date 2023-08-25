fedf4488f1677376873cf3aba4b10574101d0b16
Louis Tsang <kawingt2@gmail.com>
2023-08-26 07:33:12 +0800

    added back static files dir

--- starchaser_project/settings.py
+++ starchaser_project/settings.py
@@ -134,6 +134,8 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = 'static/'
 
+STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
