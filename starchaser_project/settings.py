5de8a25b48720c9895abcada7c8380a9abb13007
Louis Tsang <kawingt2@gmail.com>
2023-08-26 07:20:43 +0800

    added admin related files

--- starchaser_project/settings.py
+++ starchaser_project/settings.py
@@ -129,9 +129,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    str(BASE_DIR.joinpath("static"))
-]
+STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
