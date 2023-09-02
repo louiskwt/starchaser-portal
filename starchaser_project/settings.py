d3cbd7abb9f2914aa7bd3dc08ded3e2f92bc7277
Louis Tsang <kawingt2@gmail.com>
2023-09-02 21:50:49 +0800

    removed unused code

--- starchaser_project/settings.py
+++ starchaser_project/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import mimetypes
 import os
 from pathlib import Path
 
@@ -128,8 +127,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-mimetypes.add_type("text/css", ".css", True)
 
 STATIC_URL = 'static/'
 
