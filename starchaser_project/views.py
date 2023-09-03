f94339da90a8e3b46a2084c1fa9c5e2cc2910ec3
Louis Tsang <kawingt2@gmail.com>
2023-09-03 23:07:17 +0800

    cleaned up and updated title

--- starchaser_project/views.py
+++ starchaser_project/views.py
@@ -1,9 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 
 
 class LandingPageView(View):
-    
     def get(self, request):
         return render(request, "landing-page.html")
