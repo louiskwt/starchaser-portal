112bdf3d31bd22a0f5de31c906e2e22639d826bb
Louis Tsang <kawingt2@gmail.com>
2023-10-30 23:28:52 +0800

    expanded dse post model

--- dse/models.py
+++ dse/models.py
@@ -1,12 +1,38 @@
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
 class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
     title = models.CharField(max_length=256)
     body = models.TextField()
+    slug = models.SlugField()
+    description = models.TextField(max_length=256)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    published_at = models.DateField(auto_now=True)
+
+    objects = models.Manager()
+    published = PublishedManager()
+
+    class Meta:
+        ordering = ['-published_at']
+        indexes = [
+            models.Index(fields=['-published_at'])
+        ]
+
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("notes_detail", kwargs={"pk": self.pjk})
+        return reverse("notes_detail", kwargs={"pk": self.pk})
