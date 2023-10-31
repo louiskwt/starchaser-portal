171f1afe7a6943746a257ca4793278541e628967
Louis Tsang <kawingt2@gmail.com>
2023-10-31 08:23:24 +0800

    fine tuned admin

--- dse/admin.py
+++ dse/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
+
 from .models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class DsePostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'published_at', 'status', 'created_at']
+    list_filtert = ['status', 'published_at']
+    search_fields = ['title', 'body']
+    date_hierarchy = 'published_at'
+    ordering = ['status', 'published_at']
