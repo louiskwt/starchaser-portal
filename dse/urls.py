f7fabe271e80467c463cfb7b3c4f57fb05a46dd0
Louis Tsang <kawingt2@gmail.com>
2023-11-01 21:41:28 +0800

    switched to fbv for pagination

--- dse/urls.py
+++ dse/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import PostListView, notes_detail
+from .views import PostListView, notes_detail, post_list
 
 urlpatterns = [
-    path("", PostListView.as_view(), name="dse_main"),
+    path("", post_list, name="dse_main"),
     path("notes/<int:year>/<int:month>/<int:day>/<slug:post>/<int:id>/", notes_detail, name="notes_detail")
 ]
--- dse/views.py
+++ dse/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import EmptyPage, Paginator
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
 
@@ -10,6 +11,15 @@ class PostListView(ListView):
     paginate_by = 5
     template_name = "dse-home.html"
 
+def post_list(request):
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 5)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'dse-home.html', {'posts': posts})
 
 def notes_detail(request, year, month, day, post, id):
     post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, published_at__year=year, published_at__month=month, published_at__day=day, id=id)
