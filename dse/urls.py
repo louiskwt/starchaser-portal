9c4e9172143b4d405f006c56444d7d5c1e9c791a
Louis Tsang <kawingt2@gmail.com>
2023-11-10 22:45:18 +0800

    implemented tag slug fliter

--- dse/urls.py
+++ dse/urls.py
@@ -4,5 +4,6 @@ from .views import notes_detail, post_list
 
 urlpatterns = [
     path("", post_list, name="dse_main"),
+    path('/<slug:tag_slug>/', post_list, name='dse_post_list_by_tag'),
     path("notes/<int:year>/<int:month>/<int:day>/<slug:post>/<int:id>/", notes_detail, name="notes_detail")
 ]
