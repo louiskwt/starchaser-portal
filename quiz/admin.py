aa698bbebc06b26fd6d8fe239412eb13a41861aa
Louis Tsang <kawingt2@gmail.com>
2023-11-12 23:33:24 +0800

    added filter for quiz

--- quiz/admin.py
+++ quiz/admin.py
@@ -6,6 +6,7 @@ from .models import Quiz
 @admin.register(Quiz)
 class QuizAdmin(admin.ModelAdmin):
     list_display = ['title', 'created_at']
+    list_filter = ['created_at']
     search_fields = ['title', 'content']
     date_hierarchy = 'created_at'
     ordering = ['created_at']
