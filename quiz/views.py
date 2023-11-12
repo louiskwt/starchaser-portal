e7ee7540849106c04f77bb277e800c2bea418581
Louis Tsang <kawingt2@gmail.com>
2023-11-13 07:50:05 +0800

    loaded quiz data in table

--- quiz/views.py
+++ quiz/views.py
@@ -1,8 +1,13 @@
 from django.core.paginator import EmptyPage, Paginator
 from django.shortcuts import get_object_or_404, render
-from taggit.models import Tag
+
+from .models import Quiz
 
 
 def quiz_index(request):
-    return render(request, 'quiz/quiz-home.html')
+    quizzes = Quiz.objects.all().values
+    context = {
+        'quizzes': quizzes
+    }
+    return render(request, 'quiz/quiz-home.html', context)
 
