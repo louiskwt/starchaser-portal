a3abdda544bbae9ec9700a9edd0cd9493e8f1c6c
Louis Tsang <kawingt2@gmail.com>
2023-11-12 23:25:37 +0800

    defined models for quiz and ran migrations

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    slug = models.SlugField()
+    source = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        indexes = [
+            models.Index(fields=['-created_at'])
+        ]
+    
+    def __str__(self):
+        return self.title
+
