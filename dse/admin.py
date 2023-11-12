112de20bc1539ca1b6db4edabb0cfca8f1179bdb
Louis Tsang <kawingt2@gmail.com>
2023-11-12 23:32:34 +0800

    fixed dse admin filter

--- dse/admin.py
+++ dse/admin.py
@@ -6,7 +6,7 @@ from .models import Post
 @admin.register(Post)
 class DsePostAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'published_at', 'status', 'created_at']
-    list_filtert = ['status', 'published_at']
+    list_filter = ['status', 'published_at']
     search_fields = ['title', 'body']
     date_hierarchy = 'published_at'
     ordering = ['status', 'published_at']
