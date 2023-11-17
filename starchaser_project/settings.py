b73a5f72b4fdd01a37bb4c0330db2a52e66b8f7a
Louis Tsang <kawingt2@gmail.com>
2023-11-17 08:32:23 +0800

    proposed fix for admin style

--- starchaser_project/settings.py
+++ starchaser_project/settings.py
@@ -9,12 +9,14 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import mimetypes
 import os
 from pathlib import Path
 
 from dotenv import load_dotenv
 
 load_dotenv()
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
