6fb0f969945802ea33d25bfa83756438a3a6ffe8
Louis Tsang <kawingt2@gmail.com>
2023-12-03 22:41:30 +0800

    included sitemaps in url conf

--- starchaser_project/urls.py
+++ starchaser_project/urls.py
@@ -17,12 +17,19 @@ from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
 
+from dse.sitemaps import DSESitemap
+from quiz.sitemaps import QuizSitemap
 from starchaser_project.views import LandingPageView
+
+sitemaps = {
+    'dse': DSESitemap,
+    'quiz': QuizSitemap
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('dse/', include('dse.urls')),
     path('quiz/', include('quiz.urls')),
     path("", LandingPageView.as_view(), name="home"),
-    # path("sitemap.xml", sitemap, {"sitemaps", {}}, name="django.contrib.sitemaps.views.sitemap")
+    path("sitemap.xml", sitemap, {"sitemaps", sitemaps}, name="django.contrib.sitemaps.views.sitemap")
 ]
