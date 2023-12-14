899a58a9cae02d72f7f094416edb6afe5d43f5cd
Louis Tsang <kawingt2@gmail.com>
2023-12-14 21:33:11 +0800

    wrote view for ielts app

--- ielts/views.py
+++ ielts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.core.paginator import EmptyPage, Paginator
+from django.shortcuts import get_object_or_404, render
+from taggit.models import Tag
 
-# Create your views here.
+from .models import IeltsPost
+
+
+def post_list(request, tag_slug=None):
+    post_list = IeltsPost.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+
+    # Pagination
+    paginator = Paginator(post_list, 5)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'ielts/ielts-home.html', {'posts': posts, 'tag': tag})
