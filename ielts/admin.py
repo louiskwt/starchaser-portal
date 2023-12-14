cfc023bb8e4d7329496e36c03a6520e87aede5f0
Louis Tsang <kawingt2@gmail.com>
2023-12-14 21:38:49 +0800

    registered ielts post in admin

--- ielts/admin.py
+++ ielts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import IeltsPost
+
+
+@admin.register(IeltsPost)
+class DsePostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'published_at', 'status', 'created_at']
+    list_filter = ['status', 'published_at']
+    search_fields = ['title', 'body']
+    date_hierarchy = 'published_at'
+    ordering = ['status', 'published_at']
