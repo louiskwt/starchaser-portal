620e51137d2adb022d895fc92c5e9d0ac68d857d
Louis Tsang <kawingt2@gmail.com>
2023-12-19 23:33:41 +0800

    dynamically added active to nav-link with js

--- static/js/ui.js
+++ static/js/ui.js
@@ -5,4 +5,9 @@ document.addEventListener("DOMContentLoaded", () => {
   navbarBtn.addEventListener("click", () => {
     navbar.classList.toggle("collapse");
   });
+
+  const navLinks = document.querySelectorAll(".nav-link");
+  navLinks.forEach((link) => {
+    if (link.href === window.location.href) link.classList.add("active");
+  });
 });
