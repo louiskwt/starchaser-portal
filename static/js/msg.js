1a2bdeac0fffc2a409451a5455d4fad713d252b2
Louis Tsang <kawingt2@gmail.com>
2024-02-15 23:28:25 +0800

    hide main alert if seen within 48 hrs

--- static/js/msg.js
+++ static/js/msg.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   const alertBtn = document.getElementById("alert-btn");
+  const mainAlert = document.getElementById("main-alert");
+
+  const readMessageRecord = JSON.parse(localStorage.getItem("readMessage")) || null;
+  const twoDayAgo = new Date().getTime() - 1000 * 60 * 60 * 48;
+
+  if (readMessageRecord && readMessageRecord.time > twoDayAgo) mainAlert.style.display = "none";
 
   alertBtn.addEventListener("click", () => {
     const alertData = {
