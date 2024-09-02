cbf2e1a26693fe4659f6e87465cd60b89c9e4408
Louis Tsang <kawingt2@gmail.com>
2024-09-02 11:17:14 +0800

    set up flash message and handled post request in view

--- web/routes.py
+++ web/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from web import web_app
 from web.forms import LoginForm
 
@@ -7,7 +7,10 @@ def index():
     user = {'username': 'Eren'}
     return render_template('index.html', title='Home', user=user)
 
-@web_app.route('/login')
+@web_app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login successfully! Welcome {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/')
     return render_template('login.html', title='Sign In', form=form)
