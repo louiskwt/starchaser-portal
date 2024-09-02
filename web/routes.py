1ee28c5246d62482a447414383f14273333d2078
Louis Tsang <kawingt2@gmail.com>
2024-09-02 11:43:16 +0800

    used url_for for url mapping

--- web/routes.py
+++ web/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from web import web_app
 from web.forms import LoginForm
 
@@ -12,5 +12,5 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login successfully! Welcome {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect('/')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
