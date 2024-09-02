d4c340b37e859ebf1a9670f120637fdc799ee2eb
Louis Tsang <kawingt2@gmail.com>
2024-09-02 10:57:49 +0800

    set up login view

--- web/routes.py
+++ web/routes.py
@@ -1,7 +1,13 @@
 from flask import render_template
 from web import web_app
+from web.forms import LoginForm
 
 @web_app.route('/')
 def index():
     user = {'username': 'Eren'}
     return render_template('index.html', title='Home', user=user)
+
+@web_app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
