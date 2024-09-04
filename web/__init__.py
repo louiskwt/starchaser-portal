eb4098baa8c4ca07fd1b9985669f363ccaf46b92
Louis Tsang <kawingt2@gmail.com>
2024-09-04 13:46:22 +0800

    set up db

--- web/__init__.py
+++ web/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 web_app = Flask(__name__)
 web_app.config.from_object(Config)
+db = SQLAlchemy(web_app)
+migrate = Migrate(web_app, db)
 
-from web import routes
+from web import routes, models
