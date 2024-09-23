7497c0ac06580de57d34cde78e288aaa48f08754
Louis Tsang <kawingt2@gmail.com>
2024-09-23 09:44:24 +0800

    removed console logs and suspended analytics

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,5 +1,4 @@
 // Import the functions you need from the SDKs you need
-import {getAnalytics} from "firebase/analytics";
 import {initializeApp} from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -15,10 +14,8 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_APP_ID,
   measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
-console.log(firebaseConfig);
-console.log(import.meta);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 export {app};
