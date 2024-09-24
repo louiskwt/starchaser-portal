066e7ee3db6058885ebcf4287a7d87ba09d34e91
Louis Tsang <kawingt2@gmail.com>
2024-09-24 21:03:56 +0800

    added back error element

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {AuthProvider} from "./hooks";
-import {LandingPage, LoginPage, ProtectedPageLayout} from "./pages";
+import {ErrorPage, LandingPage, LoginPage, ProtectedPageLayout} from "./pages";
 
 export const App = () => {
   const AppRoute = () => (
     <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<LandingPage />} errorElement={<ErrorPage />} />
+      <Route path="/login" element={<LoginPage />} errorElement={<ErrorPage />} />
       <Route
         path="/course"
         element={
@@ -15,6 +15,7 @@ export const App = () => {
             <h1>Protected</h1>{" "}
           </ProtectedPageLayout>
         }
+        errorElement={<ErrorPage />}
       />
     </Routes>
   );
