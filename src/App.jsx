92c34c7e02c35f46ddb81f75ffffdf1f537fdc4b
Louis Tsang <kawingt2@gmail.com>
2024-09-24 21:03:14 +0800

    made protected route work

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {AuthProvider} from "./hooks";
-import {LandingPage, LoginPage} from "./pages";
+import {LandingPage, LoginPage, ProtectedPageLayout} from "./pages";
 
 export const App = () => {
   const AppRoute = () => (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route
+        path="/course"
+        element={
+          <ProtectedPageLayout>
+            {" "}
+            <h1>Protected</h1>{" "}
+          </ProtectedPageLayout>
+        }
+      />
     </Routes>
   );
 
