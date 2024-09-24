06bd0b3ad11c0b82a6a03d94c44a28e32242209c
Louis Tsang <kawingt2@gmail.com>
2024-09-24 15:55:49 +0800

    restyled error page

--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -1,16 +1,21 @@
-import {useRouteError} from "react-router-dom";
+import {Link, useRouteError} from "react-router-dom";
 
 export const ErrorPage = () => {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+    <div className="h-screen w-screen flex justify-center items-center">
+      <div className="card bg-neutral text-neutral-content w-96">
+        <div className="card-body items-center text-center">
+          <h2 className="card-title text-xl">Oops!</h2>
+          <p>Sorry, an unexpected error has occurred.</p>
+
+          <Link to="/">
+            <button className="btn btn-primary ">Go Back </button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
