d7b56182f1e752b3c2226c57fae9686cf84cb55d
Louis Tsang <kawingt2@gmail.com>
2024-09-26 16:22:44 +0800

    set up forgotpassword page

---
+++ src/pages/forgotpassword.jsx
@@ -0,0 +1,53 @@
+import {useState} from "react";
+import {Link} from "react-router-dom";
+import {useAuth} from "../hooks";
+
+export const ForgotPasswordPage = () => {
+  const {resetPassword} = useAuth();
+  const [email, setEmail] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const handleInutChange = (email) => {
+    setEmail(email);
+  };
+  const handlePasswordReset = async () => {
+    try {
+      resetPassword(email);
+    } catch (error) {
+      setErrorMessage(error.message.replace("Firebase: ", "Oops! "));
+    }
+  };
+
+  return (
+    <>
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight ">Sign in to your account</h2>
+        </div>
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm text-secondary text-center">{errorMessage}</div>
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <div className="space-y-6">
+            <label className="block form-control w-full ">
+              <div className="label">
+                <span className="label-text">Email</span>
+              </div>
+              <input name="email" type="text" placeholder="Type here" onChange={(e) => handleInutChange(e.target.value)} className="input input-bordered w-full" />
+            </label>
+            <div>
+              <button onClick={() => handlePasswordReset()} className="btn flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-lg font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                Send Reset Password Request
+              </button>
+            </div>
+          </div>
+
+          <p className="mt-10 text-center text-xl text-white">
+            <Link to="/login" className="text-primary">
+              {" "}
+              Go Back
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,9 @@
 import {CoursePage} from "./course";
 import {ErrorPage} from "./error";
+import {ForgotPasswordPage} from "./forgotpassword";
 import {LandingPage} from "./landing";
 import {LoginPage} from "./login";
 import {ProtectedPageLayout} from "./protectedPageLayout";
 import {RegisterPage} from "./register";
 
-export {CoursePage, ErrorPage, LandingPage, LoginPage, ProtectedPageLayout, RegisterPage};
+export {CoursePage, ErrorPage, ForgotPasswordPage, LandingPage, LoginPage, ProtectedPageLayout, RegisterPage};
