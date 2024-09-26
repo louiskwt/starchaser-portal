fa782c84874148e0d07224d98db94a9726a18d07
Louis Tsang <kawingt2@gmail.com>
2024-09-26 16:04:51 +0800

    handled email login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,13 +1,40 @@
+import {useState} from "react";
 import {Link, useNavigate} from "react-router-dom";
 import {useAuth} from "../hooks";
 
 export const LoginPage = () => {
-  const {loginWithGoogle, setUser} = useAuth();
+  const {loginWithGoogle, loginWithEmail, setUser} = useAuth();
   const navigate = useNavigate();
+  const [loginForm, setLoginForm] = useState({
+    email: "",
+    password: "",
+  });
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const handleInutChange = (fieldName, value) => {
+    const updatedForm = loginForm;
+    updatedForm[fieldName] = value;
+    setLoginForm(updatedForm);
+  };
   const handleGoogleLogin = async () => {
-    const user = await loginWithGoogle();
-    setUser(user);
-    navigate("/course");
+    try {
+      const user = await loginWithGoogle();
+      setUser(user);
+      navigate("/course");
+    } catch (error) {
+      setErrorMessage(error.message.replace("Firebase: ", "Oops! "));
+    }
+  };
+
+  const handleEmailLogin = async () => {
+    try {
+      const {email, password} = loginForm;
+      const user = await loginWithEmail(email, password);
+      setUser(user);
+      navigate("/course");
+    } catch (error) {
+      setErrorMessage(error.message.replace("Firebase: ", "Oops! "));
+    }
   };
   return (
     <>
@@ -15,24 +42,26 @@ export const LoginPage = () => {
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight ">Sign in to your account</h2>
         </div>
-
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm text-secondary text-center">{errorMessage}</div>
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <div className="space-y-6">
             <label className="block form-control w-full ">
               <div className="label">
                 <span className="label-text">Email</span>
               </div>
-              <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+              <input name="email" type="text" placeholder="Type here" onChange={(e) => handleInutChange(e.target.name, e.target.value)} className="input input-bordered w-full" />
             </label>
             <label className="block form-control w-full ">
               <div className="label">
                 <span className="label-text">Password</span>
               </div>
-              <input type="password" placeholder="Type here" className="input input-bordered w-full" />
+              <input name="password" type="password" placeholder="Type here" onChange={(e) => handleInutChange(e.target.name, e.target.value)} className="input input-bordered w-full" />
             </label>
 
             <div>
-              <button className="btn flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-lg font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+              <button onClick={() => handleEmailLogin()} className="btn flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-lg font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                Sign in
+              </button>
             </div>
             <div>
               <button
