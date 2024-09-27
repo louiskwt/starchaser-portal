2626a0506dfc29719d3723c210fc1d36b5aca25c
Louis Tsang <kawingt2@gmail.com>
2024-09-27 16:18:05 +0800

    configured table component

---
+++ src/components/Table/index.jsx
@@ -0,0 +1,35 @@
+export const Table = ({data, keys}) => {
+  const tableHead = keys.map((key, index) => {
+    return <th key={index}>{key}</th>;
+  });
+
+  console.log(data);
+
+  const tableRows = data.map((d, index) => {
+    return (
+      <tr key={index}>
+        <th>{index}</th>
+        {Object.keys(d).map((k, index) => {
+          return <td key={index}>{d[k]}</td>;
+        })}
+      </tr>
+    );
+  });
+  return (
+    <div className="overflow-x-auto">
+      <table className="table">
+        {/* head */}
+        <thead>
+          <tr>
+            <th></th>
+            {tableHead}
+          </tr>
+        </thead>
+        <tbody>
+          {/* rows */}
+          {tableRows}
+        </tbody>
+      </table>
+    </div>
+  );
+};
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,6 +2,7 @@ import {Dashboard} from "./Dashboard";
 import {Hero} from "./Hero";
 import {Navbar} from "./navbar";
 import {ProtectedRoute} from "./ProtectedRoute";
+import {Table} from "./Table";
 import {Toast} from "./Toast";
 
-export {Dashboard, Hero, Navbar, ProtectedRoute, Toast};
+export {Dashboard, Hero, Navbar, ProtectedRoute, Table, Toast};
