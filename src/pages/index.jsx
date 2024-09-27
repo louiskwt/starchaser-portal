7dbfab03c0f5d4c5d4589645c576193b93d455d1
Louis Tsang <kawingt2@gmail.com>
2024-09-27 10:12:34 +0800

    removed protected page layout

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,6 @@ import {LandingPage} from "./landing";
 import {LoginPage} from "./login";
 import {NotesIndexPage} from "./notesIndex";
 import {ProfilePage} from "./profile";
-import {ProtectedPageLayout} from "./protectedPageLayout";
 import {RegisterPage} from "./register";
 
-export {ErrorPage, ForgotPasswordPage, LandingPage, LoginPage, NotesIndexPage, ProfilePage, ProtectedPageLayout, RegisterPage};
+export {ErrorPage, ForgotPasswordPage, LandingPage, LoginPage, NotesIndexPage, ProfilePage, RegisterPage};
