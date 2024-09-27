105a396a55f015668c08a49ff33e9676a3ebb10d
Louis Tsang <kawingt2@gmail.com>
2024-09-27 11:07:32 +0800

    built note index page

--- src/pages/notesIndex.jsx
+++ src/pages/notesIndex.jsx
@@ -1,9 +1,10 @@
 import {Navigate} from "react-router-dom";
 import {Navbar} from "../components";
-import {useAuth} from "../hooks";
+import {useAuth, useNotes} from "../hooks";
 
 export const NotesIndexPage = () => {
   const {user} = useAuth();
+  const {notes} = useNotes();
 
   if (!user) {
     // user is not authenticated
@@ -12,21 +13,30 @@ export const NotesIndexPage = () => {
   return (
     <>
       <Navbar />
-      <h1 className="text-xl">All Notes</h1>
-      <div className="card bg-base-100 w-96 shadow-xl">
-        <figure>
-          <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">
-            Shoes!
-            <div className="badge badge-secondary">NEW</div>
-          </h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <div className="badge badge-outline">Fashion</div>
-            <div className="badge badge-outline">Products</div>
-          </div>
+      <div className="flex flex-col items-center">
+        <h1 className="text-2xl font-bold mb-4">Notes</h1>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-5 w-full max-w-6xl mt-4">
+          {notes.map((note, index) => (
+            <div key={note.id} className="card bg-base-100 w-80 shadow-xl mx-auto">
+              <figure>
+                <img src={note.cover} alt="Cover" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">
+                  {note.title}
+                  {index === 0 && <div className="badge badge-secondary">NEW</div>}
+                </h2>
+                <p>{note.desc}</p>
+                <div className="card-actions justify-end">
+                  {note.categories.map((c, index) => (
+                    <div key={index} className="badge badge-outline">
+                      {c}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </>
