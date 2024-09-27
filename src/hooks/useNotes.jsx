1f5c434fe84b2a95b621094abdf2735b4f9e8861
Louis Tsang <kawingt2@gmail.com>
2024-09-27 10:11:34 +0800

    add drawer to notes provider

--- src/hooks/useNotes.jsx
+++ src/hooks/useNotes.jsx
@@ -1,5 +1,6 @@
 import {collection, getDocs, getFirestore} from "firebase/firestore";
 import {createContext, useContext, useEffect, useState} from "react";
+import {Link} from "react-router-dom";
 import {firebase} from "../Firebase";
 import {useAuth} from "./useAuth";
 
@@ -36,7 +37,35 @@ export const NotesProvider = ({children}) => {
     }
   }, [user]);
 
-  return <NotesContext.Provider value={{notes, setNotes, loading, error}}>{children}</NotesContext.Provider>;
+  return (
+    <NotesContext.Provider value={{notes, setNotes, loading, error}}>
+      {children}
+      <div className="drawer">
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">{children}</div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+
+          <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-8">
+            <Link className="text-xl mb-4" to="/notes">
+              Notes
+            </Link>
+            {loading ? (
+              <span className="loading loading-spinner loading-lg"></span>
+            ) : (
+              notes.map((note) => {
+                return (
+                  <li key={note.id} className="text-lg mb-4">
+                    <a>{note.title}</a>
+                  </li>
+                );
+              })
+            )}
+          </ul>
+        </div>
+      </div>
+    </NotesContext.Provider>
+  );
 };
 
 export const useNotes = () => {
