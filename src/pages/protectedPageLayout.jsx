0cb0186d2d47adf5a32b33d6a7a3b1fa040def29
Louis Tsang <kawingt2@gmail.com>
2024-09-27 09:25:30 +0800

    load list in drawer

--- src/pages/protectedPageLayout.jsx
+++ src/pages/protectedPageLayout.jsx
@@ -1,6 +1,8 @@
 import {Navbar} from "../components";
+import {useNotes} from "../hooks";
 
 export const ProtectedPageLayout = ({children}) => {
+  const {notes} = useNotes();
   return (
     <>
       <Navbar />
@@ -10,12 +12,13 @@ export const ProtectedPageLayout = ({children}) => {
         <div className="drawer-side">
           <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
           <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-            <li>
-              <a>Sidebar Item 1</a>
-            </li>
-            <li>
-              <a>Sidebar Item 2</a>
-            </li>
+            {notes.map((note) => {
+              return (
+                <li key={note.id}>
+                  <a>{note.title}</a>
+                </li>
+              );
+            })}
           </ul>
         </div>
       </div>
