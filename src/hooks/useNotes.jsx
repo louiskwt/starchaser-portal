1bb9baf29e172187f7771149aa674d6be278a114
Louis Tsang <kawingt2@gmail.com>
2024-09-27 09:25:18 +0800

    refactored note provider and added comment

--- src/hooks/useNotes.jsx
+++ src/hooks/useNotes.jsx
@@ -1,6 +1,7 @@
 import {collection, getDocs, getFirestore} from "firebase/firestore";
 import {createContext, useContext, useEffect, useState} from "react";
 import {firebase} from "../Firebase";
+import {useAuth} from "./useAuth";
 
 const NotesContext = createContext();
 
@@ -8,27 +9,34 @@ export const NotesProvider = ({children}) => {
   const [notes, setNotes] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  const {user} = useAuth();
+
   const db = getFirestore(firebase);
 
   useEffect(() => {
-    const fetchNotes = async () => {
-      try {
-        const notesCollection = collection(db, "notes"); // Replace 'notes' with your collection name
-        const notesSnapshot = await getDocs(notesCollection);
-        const notesList = notesSnapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-        setNotes(notesList);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-  });
+    if (user) {
+      const fetchNotes = async () => {
+        try {
+          const notesCollection = collection(db, "notes"); // need to think about pagination when it scale
+          const notesSnapshot = await getDocs(notesCollection);
+          const notesList = notesSnapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }));
+          console.log(notesList);
+          setNotes(notesList);
+        } catch (err) {
+          setError(err.message);
+        } finally {
+          setLoading(false);
+        }
+      };
+      fetchNotes();
+    }
+  }, [user]);
 
-  return <NotesContext.Provider value={{notes, setNotes}}>{children}</NotesContext.Provider>;
+  return <NotesContext.Provider value={{notes, setNotes, loading, error}}>{children}</NotesContext.Provider>;
 };
 
 export const useNotes = () => {
