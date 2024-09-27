7105a112f4f50079ad15ea5e27a217f233e90ac8
Louis Tsang <kawingt2@gmail.com>
2024-09-27 12:36:11 +0800

    improved menu rendering

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,7 +2,40 @@ import {Link} from "react-router-dom";
 import {useAuth} from "../../hooks";
 
 export const Navbar = () => {
-  const {user} = useAuth();
+  const {user, loading} = useAuth();
+
+  const menu = () => {
+    if (loading)
+      return (
+        <div className="flex items-center justify-center">
+          <div className="loader">
+            <span className="loading loading-spinner loading-sm"></span>
+          </div>
+        </div>
+      );
+
+    return user ? (
+      <>
+        {" "}
+        <label htmlFor="my-drawer" className="btn btn-primary drawer-button">
+          <i className="fa-solid fa-book fa-2x"></i>
+        </label>
+        <div className="btn avatar">
+          <div className="w-12 ml-1 rounded-2xl">
+            <Link to="/profile">
+              <img src={user.photoURL} />
+            </Link>
+          </div>
+        </div>
+      </>
+    ) : (
+      <>
+        <li className="text-xl btn btn-primary">
+          <Link to="/login">Login</Link>
+        </li>
+      </>
+    );
+  };
 
   return (
     <div className="navbar bg-base-100">
@@ -10,29 +43,7 @@ export const Navbar = () => {
         <a className="btn btn-ghost text-xl">DSE Starchaser ⭐️</a>
       </div>
       <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          {user ? (
-            <>
-              {" "}
-              <label htmlFor="my-drawer" className="btn btn-primary drawer-button">
-                <i className="fa-solid fa-book fa-2x"></i>
-              </label>
-              <div className="btn avatar">
-                <div className="w-12 ml-1 rounded-2xl">
-                  <Link to="/profile">
-                    <img src={user.photoURL} />
-                  </Link>
-                </div>
-              </div>
-            </>
-          ) : (
-            <>
-              <li className="text-xl btn btn-primary">
-                <Link to="/login">Login</Link>
-              </li>
-            </>
-          )}
-        </ul>
+        <ul className="menu menu-horizontal px-1">{menu()}</ul>
       </div>
     </div>
   );
