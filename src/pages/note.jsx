1c1e2dd44f89bbecd15e6efd68067ee938bcef4d
Louis Tsang <kawingt2@gmail.com>
2024-09-27 13:00:34 +0800

    scaffolded note page

--- src/pages/note.jsx
+++ src/pages/note.jsx
@@ -1,7 +1,51 @@
 import {useParams} from "react-router-dom";
+import {useNotes} from "../hooks";
 
 export const NotePage = () => {
   const {noteId} = useParams();
-  console.log("note", noteId);
-  return <></>;
+  const {notes} = useNotes();
+  const note = notes.find((n) => (n.id = noteId));
+  console.log(note);
+
+  return (
+    <>
+      <div className="flex flex-col justify-center w-full p-12">
+        <div className="w-full divide-y-8 mb-4">
+          <h1 className="text-3xl font-bold text-left">{note.title}</h1>
+        </div>
+        <div className="w-full" id="main-topic">
+          <h2 className="text-2xl font-bold text-left">{note.topic.heading}</h2>
+          <ol className="list-decimal list-inside mt-2 mb-4">
+            {note.topic.ideas.map((idea, index) => (
+              <li className="mb-2" key={index}>
+                {idea}
+              </li>
+            ))}
+          </ol>
+          <h2 className="text-xl font-semibold mb-4">Resources</h2>
+          <ul>
+            {Object.keys(note.topic.resources).map((key, id) => {
+              return (
+                <li key={id} className="link">
+                  <a href={note.topic.resources[key]}>{key}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="mt-8" id="vocabulary">
+          <h2 className="text-2xl font-bold text-left">Vocab Table</h2>
+        </div>
+        <div className="mt-8" id="grammar">
+          <h2 className="text-2xl font-bold text-left">Grammar Section</h2>
+        </div>
+        <div className="mt-8" id="video">
+          <h2 className="text-2xl font-bold text-left">Video</h2>
+        </div>
+        <div className="mt-8" id="link">
+          <h2 className="text-2xl font-bold text-left">Link to Notes</h2>
+        </div>
+      </div>
+    </>
+  );
 };
