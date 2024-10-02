c4558a8233cd6f33e873c11078507e0dc81820c1
Louis Tsang <kawingt2@gmail.com>
2024-10-02 15:55:45 +0800

    built separate spinner

---
+++ src/components/Spinner/index.jsx
@@ -0,0 +1,9 @@
+export const Spinner = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="loader">
+        <span className="loading loading-spinner loading-lg"></span>
+      </div>
+    </div>
+  );
+};
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,7 +3,8 @@ import {Hero} from "./Hero";
 import {Navbar} from "./navbar";
 import {NoteSection} from "./NoteSection";
 import {ProtectedRoute} from "./ProtectedRoute";
+import {Spinner} from "./Spinner";
 import {Table} from "./Table";
 import {Toast} from "./Toast";
 
-export {Dashboard, Hero, Navbar, NoteSection, ProtectedRoute, Table, Toast};
+export {Dashboard, Hero, Navbar, NoteSection, ProtectedRoute, Spinner, Table, Toast};
