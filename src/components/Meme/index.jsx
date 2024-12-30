c6b31fb7a95b9596f45f835309be3a5a823d367e
Louis Tsang <kawingt2@gmail.com>
2024-12-30 14:16:57 +0800

    removed all to start over

--- src/components/Meme/index.jsx
+++
@@ -1,3 +0,0 @@
-export const Meme = ({text}) => {
-  return <img src={`https://api.memegen.link/images/custom/Ahhhh/${text}.jpg?background=https://i.imgur.com/CsCgN7Ll.png&width=400`} />;
-};
--- src/hooks/index.jsx
+++
@@ -1,5 +0,0 @@
-import {AuthProvider, useAuth} from "./useAuth";
-import {NotesProvider, useNotes} from "./useNotes";
-import {ToastProvider, useToast} from "./useToast";
-
-export {AuthProvider, NotesProvider, ToastProvider, useAuth, useNotes, useToast};
